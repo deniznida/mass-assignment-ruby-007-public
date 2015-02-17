52ee6695f9dd1cca17d083969c89dbd2dfebee14
deniznida <denizunat@hotmail.com>
2015-02-17 14:24:32 -0500

    done

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,10 @@
 class Person
   # code here
+
+  def initialize (attributes)
+    attributes.each do |k, v|
+      self.class.send(:attr_accessor, k)
+      self.send("#{k}=", v)
+    end
+  end
 end
